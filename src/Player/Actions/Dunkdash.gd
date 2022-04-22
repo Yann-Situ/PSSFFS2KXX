95e65adb92ce07583fbc4b95533f3abfedafdf75
Yann-Situ <yann-situ.gazull@ens-lyon.fr>
2022-04-22 18:49:59 +0200

    [ADD] new animations + backgrounds

--- src/Player/Actions/Dunkdash.gd
+++ src/Player/Actions/Dunkdash.gd
@@ -5,6 +5,7 @@ export var can_go_delay = 0.2#s
 export(Color, RGBA) var ghost_modulate
 
 var velocity_save = Vector2.ZERO
+var dash_dir = Vector2.ZERO
 
 func move(delta):
 	print("begindash")
@@ -19,7 +20,7 @@ func move(delta):
 
 	P.PlayerEffects.cloud_start()
 	P.PlayerEffects.jump_start()
-	P.PlayerEffects.ghost_start(0.18,0.04, ghost_modulate)
+	P.PlayerEffects.ghost_start(0.21,0.05, ghost_modulate)
 	P.PlayerEffects.distortion_start("fast_soft", 0.75)
 	S.get_node("ToleranceDunkJumpPressTimer").stop()
 	S.get_node("CanJumpTimer").start(S.jump_countdown)
@@ -31,18 +32,27 @@ func move(delta):
 #	q *= -P.dunkdash_speed/q.length()
 #	q.y = max(q.y, P.jump_speed)
 #	P.get_out(P.global_position, q)
-	var q = (S.dunkdash_basket.position - P.position).normalized()
-	q *= max(P.dunkdash_speed, velocity_save.dot(q))
-	P.get_out(P.global_position, q)
+	dash_dir = (S.dunkdash_basket.position - P.position).normalized()
+	var q = max(P.dunkdash_speed, velocity_save.dot(dash_dir)) * dash_dir
+	if not S.is_grinding:
+		P.get_out(P.global_position, q)
+	else :
+		S.velocity = q
 	#print("Velocity: "+str(S.velocity))
 	P.get_node("Camera").screen_shake(0.2,10)
 
 func move_end():
 	print("enddash")
 	P.gravity = P.based_gravity
-	if S.direction_p * S.move_direction < 0:
-		S.velocity.x = 0.0
-	else :
-		S.velocity.x *= 0.5
-	S.velocity.y *= 0.5
+	
+	if not S.is_grinding:
+		var vel_dir = S.velocity.normalized() # vel_dir is not always equals to dash_dir (e.g if there is an obstacle on the dash path)
+		var temp = velocity_save.dot(vel_dir)
+		if 0.25*S.velocity.length_squared() > temp*temp :
+			S.velocity *= 0.5
+		else :
+			S.velocity = temp * vel_dir
+		
+		if S.direction_p * S.move_direction < 0:
+			S.velocity.x = 0.0
 	#P.get_node("Sprite").modulate = Color.white
