ba082797fbc738f490c99336c0a5ad1e4f16b787
Yann-Situ <yannsitu@gmail.com>
2022-07-30 13:27:43 +0200

    [ADD] parabola catenary scenery objects

---
+++ src/Items/Environment/ParabolaCatenary.gd
@@ -0,0 +1,63 @@
+extends Path2D
+
+export(float) var p0_slope = 0.0 # pix/pix
+export(int, 2, 1024) var nb_line_points = 30
+
+var p0 : Vector2 # first end point
+var p1 : Vector2 # second end point (p0.x <= p1.x)
+onready var line : Line2D = $Line2D 
+
+# the parabola is parametrised as follows: y = k(x - pmin.x)^2 + pmin.y
+var pmin : Vector2
+var k : float # pix-1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update_endpoints()
+	compute_parabola_parameters()
+	update_Line2D_points()
+
+func update_endpoints():
+	# also ensure p0.x <= p1.x
+	p0 = self.curve.get_point_position(0)
+	p1 = self.curve.get_point_position(1)
+	if p0.x > p1.x:
+		var temp = p1
+		p1 = p0
+		p0 = temp
+
+func compute_parabola_parameters() -> bool:
+	# compute pmin and k: the parabola is parametrised as : y = k(x - pmin.x)^2 + pmin.y
+	# return if the parameters are valid
+	var H = p1.y - p0.y
+	var L = p1.x - p0.x
+	if L == 0.0:
+		push_warning("ParabolaCatenary invalid parameters")
+		return false
+	var invL = 1.0/L
+	var kL = H*invL - p0_slope
+	if kL == 0.0:
+		push_warning("ParabolaCatenary invalid parameters")
+		return false
+	k = kL*invL
+	var H_above_KL = H/kL
+	pmin.x = 0.5*(p1.x + p0.x - H_above_KL)
+	var temp = 0.5*(H_above_KL - L)
+	pmin.y = p0.y - k*temp*temp
+	return true
+
+func parabola(x : float):
+	var temp = (x - pmin.x)
+	return k*temp*temp + pmin.y
+
+func update_Line2D_points():
+	# update the Line2D points
+	line.clear_points()
+	var p = p0
+	line.add_point(p)
+	var increment = (p1.x - p0.x)/nb_line_points
+	for i in range(nb_line_points):
+		p.x = p.x + increment
+		p.y = parabola(p.x)
+		line.add_point(p)
